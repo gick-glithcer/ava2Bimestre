07d8e81854fde9655d10017f842a33801c2aa8b8
gick <samuelffdima2@gmail.com>
2026-06-03 02:23:54 +0000

    v5

--- calc_imc.py
+++ calc_imc.py
@@ -1,18 +1,17 @@
 #Wylamys:lógica matematica
 def calcular_imc(peso, altura):
-    imc = peso / (altura * altura)
-    return imc
+    return peso / (altura ** 2)
 
 
-#Arthur: Classificador de dados
-def classficar(valor_imc):
-    if valor_imc <25:
+#Arthur:Classificador de dados
+def classificar(valor_imc):
+    if valor_imc < 25:
         return "NORMAL"
     else:
         return "ACIMA DO PESO"
     
 
-#Mauricio: Especialista em Conteúdo
+#Mauricio:Especialista em Conteúdo
 def gerar_aviso(status):
     if status == "NORMAL":
         return "Parabéns! Continue mantendo hábitos saudáveis."
@@ -20,13 +19,14 @@ def gerar_aviso(status):
         return "Atenção! Considere melhorar a alimentação e praticar atividades físicas."
 
 
-#Samuel: Engenheiro de Integração
-peso = float(input("Digite seu peso (kg):"))
-altura = float(input("Digite sua altura (m):"))
-imc = calcular_imc(peso,altura)
-status = classficar(imc)
-aviso = gerar_aviso(status)
-print("\n=== RESULTADO DA AVALIAÇÃO ===")
-print(f"IMC:{imc:.2f}")
-print(f"Classificação:{status}")
-print(f"Recomendação:{aviso}")
+#Samuel:Engenheiro de Integração
+altura = (input("Digite sua Altura(m)"))
+peso = float(input("Digite seu Peso(kg)"))
+
+imc = calcular_imc(peso, altura)
+classificacao = classficar(imc)
+status = gerar_aviso(classificacao)
+
+print(f"\nResultado da Avaliação: {imc:.2f}")
+print(f"Classificar dados: {classificacao}")
+print(f"Recomendação: {status}")
